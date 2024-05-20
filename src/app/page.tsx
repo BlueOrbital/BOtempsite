38115fe44b2feb288e342f16b97eabbfb53d508b
Ryo_Code <107195296+KiyberCo@users.noreply.github.com>
2024-05-20 23:41:42 +0100

    Day 1 - Font, BG gradient, animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-row max-w-100vw h-fit-content items-start justify-center p-24">
+      <section className="flex flex-column flex-wrap z-10 h-screen w-full max-w-7xl items-start justify-between">
+        <div className="flex flex-row flex-wrap h-auto w-full items-start justify-center">
+          <h1 className="w-full h-full mr-auto font-secondary text-title">
+            Welcome to Blue Orbital
+          </h1>
+          <h2 className="w-auto h-full font-main text-sub-title typewriter animate-typing">
+            Bespoke Web Design
+          </h2>
+          {/* <Image
+            className="w-auto h-auto place-items-center gap-2 p-1 ml-auto"
+            src="/BO_notxt_trans_white .png"
+            alt="Blue Orbital Logo"
+            // className="dark:invert"
+            width={80}
+            height={90}
+            priority
+          /> */}
+        </div>      
+        <div className="radial before:from-white-x-1/3 a-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+          <Image
+            className="relative"
+            src="/BO_logo_white_trans.png"
+            alt="Large Blue Orbital Logo"
+            width={210}
+            height={70}
+            priority
+          />
         </div>
-      </div>
+      </section>
+      {/* <section className="z-10 h-screen w-full max-w-5xl items-start justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-centre border-b">
+          Our site is currently under development, but we are not far off now... 
+        </p>
+      </section> */}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
+      {/*
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -107,7 +108,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,42 +8,45 @@ const config: Config = {
   ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      deepGrey: '#141A1F',
-      darkGrey: '#242F38',
-      midGrey: '#88A0B5',
-      lightGrey: '#B0C0CF',
-      deepBlue: '#081521',
-      darkBlue: '#0D2336',
-      midBlue: '#1C374F',
-      lightBlue: '#BFE1FF',
-      darkGreyBlue: '#2F4E69',
-      midGreyBlue: '#486782',
-      lightGreyBlue: '#C5D8E8',
+      deepGrey: "#141A1F",
+      darkGrey: "#242F38",
+      midGrey: "#88A0B5",
+      lightGrey: "#B0C0CF",
+      deepBlue: "#081521",
+      darkBlue: "#0D2336",
+      midBlue: "#1C374F",
+      lightBlue: "#BFE1FF",
+      darkGreyBlue: "#2F4E69",
+      midGreyBlue: "#486782",
+      lightGreyBlue: "#C5D8E8",
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      main: ["Kumbh_Sans", "sans-serif"],
+      secondary: ["Orbitron", "sans-serif"],
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '144': '36rem',
+        "128": "32rem",
+        "144": "36rem",
+      },
+      fontSize: {
+        "title": "4rem",
+        "sub-title": "2.5rem",
       },
       borderRadius: {
-        '4xl': '2rem',
+        "4xl": "2rem",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "typing": "typing 1.5s steps(40, end)",
+        "blink-caret": "blink-caret .75s step-end infinite",
       },
-    }
+    },
   },
   plugins: [],
 };
