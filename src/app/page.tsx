4a26dd7d9d3c0117142d0722b1a12e38663562c3
Ryo_Code <107195296+KiyberCo@users.noreply.github.com>
2024-05-21 17:30:07 +0100

    Updates layout to be more vertical appeasing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             Bespoke Web Design.
           </h2>
         </div>
-        <div className="w-auto h-auto max-w-2/3 mx-auto mt-16 mb-12 self-center">
+        <div className="w-auto h-auto max-w-2/3 mx-auto my-12 self-center">
           <Image
             className="relative animate-fade-in"
             src="/BO_logo_white_trans.png"
@@ -23,8 +23,8 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-row flex-wrap h-auto w-full items-center justify-center">
-          <h2 className="w-auto h-full font-main text-3xl pl-3 xl:text-left sm:text-center">
-            Our site is currently in development, come back again soon
+          <h2 className="w-full h-full font-main text-3xl text-center mb-8">
+            Our site is currently in development, try back again soon
           </h2>
           <h2 className="w-auto h-full font-main text-3xl dotdot pl-3 text-center self-center">
             ...
