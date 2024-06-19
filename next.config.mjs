2faff37593376439a97a15e10b47a5f50135b862
Ryo_Code <107195296+KiyberCo@users.noreply.github.com>
2024-06-19 16:42:27 +0100

    Fixes SVG issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 // next.config.js
 import withImages from 'next-images';
 
-export default withImages({
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test?.test?.('.svg')
-    );
-
-    // Add SVGR loader to the existing rule
-    fileLoaderRule.exclude = /\.svg$/;
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-});
+export default withImages();
 
