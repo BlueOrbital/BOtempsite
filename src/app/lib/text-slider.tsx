60d050227eace048f205b50194b4c138f01a4040
Ryo_Code <107195296+KiyberCo@users.noreply.github.com>
2024-06-20 10:23:07 +0100

    Fixes build issues with "JSX" typeface and dependency warning with UseEffect

--- src/app/lib/text-slider.tsx
+++ src/app/lib/text-slider.tsx
@@ -1,6 +1,7 @@
 import { fadeAnimationSelector, homeMessages } from "./staticArrays";
 import { reorderList } from "./utilities";
 import React, { useEffect, useState } from "react";
+import type { JSX } from "react";
 
 interface TSProps {
   repeat: number;
@@ -9,55 +10,53 @@ interface TSProps {
 export default function TextSlider(
   props: TSProps,
 ): React.FunctionComponentElement<JSX.Element> {
-    const [content, setContent] = useState<Array<undefined | JSX.Element>>([]);
-    const printDiv = (repeat: number): Array<JSX.Element> => {
+  const [content, setContent] = useState<Array<undefined | JSX.Element>>([]);
+  const printDiv = (repeat: number): Array<JSX.Element> => {
     const element: Array<JSX.Element> = [];
     for (let i = 0; i < repeat - 1; i++) {
-        const reorderedList = reorderList(homeMessages);
-        element.push(
+      const reorderedList = reorderList(homeMessages);
+      element.push(
         <div
-            key={`#div${element.length}${i}`}
-            className="first:-mt-10 flex flex-row flex-nowrap h-fit w-auto overflow-clip"
+          key={`#div${element.length}${i}`}
+          className="first:-mt-10 flex flex-row flex-nowrap h-fit w-auto overflow-clip"
         >
-            {reorderedList.map((message: string, index: number) => (
-            <React.Fragment
-                key={`${element.length}-${i}-index-${index}`}
-            >
-                {index % 2 !== 0 && (
+          {reorderedList.map((message: string, index: number) => (
+            <React.Fragment key={`${element.length}-${i}-index-${index}`}>
+              {index % 2 !== 0 && (
                 <h2
-                    className={`${fadeAnimationSelector[Math.floor(Math.random()*4)]} opacity-0 inline mx-2 font-thin min-w-fit overflow-clip}`}
+                  className={`${
+                    fadeAnimationSelector[Math.floor(Math.random() * 4)]
+                  } opacity-0 inline mx-2 font-thin min-w-fit overflow-clip}`}
                 >
-                    {message}
+                  {message}
                 </h2>
-                )}
-                {index % 2 === 0 && (
+              )}
+              {index % 2 === 0 && (
                 <h2
-                    className={`${fadeAnimationSelector[Math.floor(Math.random()*4)]} opacity-0 inline mx-2 font-light min-w-fit overflow-clip}`}
+                  className={`${fadeAnimationSelector[Math.floor(Math.random() * 4)]} opacity-0 inline mx-2 font-light min-w-fit overflow-clip}`}
                 >
-                    {message}
+                  {message}
                 </h2>
-                )}
+              )}
             </React.Fragment>
-            ))}
-        </div>
-        );
+          ))}
+        </div>,
+      );
     }
     return element;
-    };
+  };
 
-    useEffect(():void => {
-        return (
-            setContent(printDiv(props.repeat))
-        );
-    },[]);
+  useEffect((): void => {
+    return setContent(printDiv(props.repeat));
+  }, [props.repeat]);
 
   return (
     <>
-        {content !== undefined && (
-            <div className="flex flex-row flex-wrap content-start items-start align-top h-fit w-auto mx-auto justify-evenly overflow-clip">
-                {content}
-            </div>
-        )}
+      {content !== undefined && (
+        <div className="flex flex-row flex-wrap content-start items-start align-top h-fit w-auto mx-auto justify-evenly overflow-clip">
+          {content}
+        </div>
+      )}
     </>
   );
 }
